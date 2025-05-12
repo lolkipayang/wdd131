5c6ed290196546040c87e21f95e7c895a5f6ed33
lolkipayang <llolkipayang@byupathway.edu>
2025-05-12 09:23:31 +0300

    Fixed rubrics

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,12 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the current year for the copyright
-    const currentYearSpan = document.getElementById('currentYear');
-    if (currentYearSpan) {
-        const currentYear = new Date().getFullYear();
-        currentYearSpan.textContent = currentYear;
-    }
-
-    // Get the last modified date of the document
     const lastModifiedSpan = document.getElementById('lastModified');
     if (lastModifiedSpan) {
         lastModifiedSpan.textContent = document.lastModified;
