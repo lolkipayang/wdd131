50f88f84542e398a6600da7c468e08030f6d0dee
lolkipayang <llolkipayang@byupathway.edu>
2025-05-13 18:09:00 +0300

    revision

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const lastModifiedSpan = document.getElementById('lastModified');
-    if (lastModifiedSpan) {
-        lastModifiedSpan.textContent = document.lastModified;
-    }
-});
+const today = new Date();
+const options = {
+    month: '2-digit',
+    day: '2-digit',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZoneName: 'short' // Added for timezone
+};
+
+full.innerHTML = `<span class="highlight">${new Intl.DateTimeFormat(
+    "en-US",
+    options
+).format(today)}</span>`;
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
