ac39d3bb1f6c6c9959057ffb06e230b25bbc3244
lolkipayang <llolkipayang@byupathway.edu>
2025-06-19 18:50:54 +0300

    CHANGED FOOTER

--- project/scripts/site.js
+++ project/scripts/site.js
@@ -40,3 +40,19 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+const today = new Date();
+const options = {
+    month: '2-digit',
+    day: '2-digit',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZoneName: 'short' // Added for timezone
+};
+
+full.innerHTML = `<span class="highlight">${new Intl.DateTimeFormat(
+    "en-US",
+    options
+).format(today)}</span>`;
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
